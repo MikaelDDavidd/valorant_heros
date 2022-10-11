8fac1031187c2b9188dff6bfff8a3ef10be98b92
Kael David <mikaeldavidlopes@gmail.com>
2022-10-11 16:39:32 -0300

    minimized

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:vertical_card_pager/vertical_card_pager.dart';
 
+import 'models/hero_model.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -29,16 +31,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<String> titles = [
-    "JETT",
-    "KILLJOY",
-    "SAGE",
-    "SOVA",
-    "VIPER",
-    "RAZE",
-    "YORU",
-    "BREACH",
+  final List<HeroModel> heros = [
+    HeroModel("JETT", "images/jett_valo.gif"),
+    HeroModel("KILLJOY", "images/killjoy_valo.gif"),
+    HeroModel("SAGE", "images/sage_valo.gif"),
+    HeroModel("SOVA", "images/sova_valo.gif"),
+    HeroModel("VIPER", "images/viper_valo.gif"),
+    HeroModel("RAZE", "images/raze_valo.gif"),
+    HeroModel("YORU", "images/yoru_valo.gif"),
+    HeroModel("BREACH", "images/breach_valo.gif"),
+    HeroModel("ASTRA", "images/astra_valo.gif"),
+    HeroModel("CYPHER", "images/cypher_valo.gif"),
+    HeroModel("OMEN", "images/omen_valo.gif"),
+    HeroModel("PHOENIX", "images/phoenix_valo.gif"),
+    HeroModel("SKYE", "images/skye_valo.gif"),
   ];
+
+  //final List<Widget> images2 =
 
   final List<Widget> images = [
     Hero(
@@ -121,6 +130,56 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     ),
+    Hero(
+      tag: "ASTRA",
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: Image.asset(
+          "images/astra_valo.gif",
+          fit: BoxFit.cover,
+        ),
+      ),
+    ),
+    Hero(
+      tag: "CYPHER",
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: Image.asset(
+          "images/cypher_valo.gif",
+          fit: BoxFit.cover,
+        ),
+      ),
+    ),
+    Hero(
+      tag: "OMEN",
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: Image.asset(
+          "images/omen_valo.gif",
+          fit: BoxFit.cover,
+        ),
+      ),
+    ),
+    Hero(
+      tag: "PHOENIX",
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: Image.asset(
+          "images/phoenix_valo.gif",
+          fit: BoxFit.cover,
+        ),
+      ),
+    ),
+    Hero(
+      tag: "SKYE",
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: Image.asset(
+          "images/skye_valo.gif",
+          fit: BoxFit.cover,
+        ),
+      ),
+    ),
   ];
 
   @override
@@ -144,8 +203,20 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         Expanded(
             child: VerticalCardPager(
-          titles: titles,
-          images: images,
+          titles: [for (var hero in heros) hero.title],
+          images: [
+            for (var hero in heros)
+              Hero(
+                tag: hero.title,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: Image.asset(
+                    hero.image,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+          ],
           textStyle:
               const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           initialPage: 2,
---
+++ lib/models/hero_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class HeroModel {
+  late String title;
+  late String image;
+
+  HeroModel(this.title, this.image);
+}
